3d51f7e8e31a47b4f6b164a38ff3511a8d028249
TaniaV <trvworking@gmail.com>
2025-03-22 16:45:02 +0000

    fixed: coin selector

--- ExercicioReact/React-router/pages/CurrencyCountryList.jsx
+++ ExercicioReact/React-router/pages/CurrencyCountryList.jsx
@@ -4,49 +4,56 @@ import { Link } from "react-router";
 
 export default function CurrencyCountryList({ currency, countryName }) {
   const [countriesListCurrency, setCountriesCurrency] = useState([]);
-  const [selectedCurrency, setSelectedCurrency] = useState();
+  const [selectedCurrency, setSelectedCurrency] = useState(currency);
 
   const getCountryCurrency = async () => {
     const response = await fetch(
-      `https://restcountries.com/v3.1/currency/${currency}`
+      `https://restcountries.com/v3.1/currency/${selectedCurrency}`
     );
     const countriesCurrency = await response.json();
     setCountriesCurrency(countriesCurrency);
+  //  console.log("countries currency", countriesCurrency);
   };
 
   useEffect(() => {
     getCountryCurrency();
-  }, []);
+  }, [selectedCurrency]);
 
-  //Poner el onChange a funcionar
   return (
     <>
-    <div>
-      <select name="" id="" onChange={(e)=>{setCountriesCurrency(e.target.value)}}>
+      <div>
+        <select
+          name=""
+          id=""
+          onChange={(e) => {
+            setSelectedCurrency(e.target.value);
+          }}
+        >
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="BRL">BRL</option>
         </select>
-        </div>
-    {/* <CurrencySelector onChange=/> */}
-    <div className="grid">
-      {countriesListCurrency.map((countryCurrency) => {
-        if (countryCurrency.name.common !== countryName) {
-          return (
-            <Link to={`/country/${countryCurrency.name.common}`} key={countryCurrency.cca3}>
+      </div>
 
-            <SmallCard
-              key={countryCurrency.cca3}
-              name={countryCurrency.name.common}
-              flag={countryCurrency.flags.svg}
-              currency={currency}
-            />
-            </Link>
-          );
-        }
-      })}
-    </div>
+      <div className="grid">
+        {countriesListCurrency.map((countryCurrency) => {
+          if (countryCurrency.name.common !== countryName) {
+            return (
+              <Link
+                to={`/country/${countryCurrency.name.common}`}
+                key={countryCurrency.cca3}
+              >
+                <SmallCard
+                  key={countryCurrency.cca3}
+                  name={countryCurrency.name.common}
+                  flag={countryCurrency.flags.svg}
+                  currency={selectedCurrency}
+                />
+              </Link>
+            );
+          }
+        })}
+      </div>
     </>
-   
   );
 }
